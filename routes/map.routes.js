f9cf9b89c4a5bfb3184779359eb9a99c00213f2c
illocarlos <carlosalbendiz93@gmail.com>
2023-08-16 19:03:38 +0200

    map

--- routes/map.routes.js
+++ routes/map.routes.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const router = express.Router();
-
+const Event = require("../models/event.model");
 
 router.get("/map", (req, res, next) => {
 
     res.render("maps/map");
 });
+
+
+router.get("/mapId", (req, res, next) => {
+
+    const { id: event_id } = req.params
+
+    //aqui conseguis el festival lo buscais en bbdd, y le pasais a la vista el objeto entero 
+    Event
+        .findById(event_id)
+        .then(event => res.render('map/mapId', event))
+        .catch(err => (console.log(err)))
+
+    res.render("maps/mapId")
+});
+
 
 module.exports = router;
